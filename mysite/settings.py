98335b8f8636a8fa2124c7585467e9495b9dc033
Shintakumi <shincode0712@gmail.com>
2021-04-18 21:23:18 +0900

    Change settings.py file to remove add DIRFILE

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join('blog', 'templates')],#ここを追加
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
